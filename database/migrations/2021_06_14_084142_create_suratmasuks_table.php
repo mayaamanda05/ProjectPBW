cc040312d71937d11b5dac0646c94069ad0c5617
mayaamanda05 <82106729+mayaamanda05@users.noreply.github.com>
2021-06-17 01:11:07 +0700

    Add files via upload

---
+++ database/migrations/2021_06_14_084142_create_suratmasuks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuratmasuksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suratmasuks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('no_surat');
+            $table->string('judul');
+            $table->string('pengirim');
+            $table->string('tanggal_masuk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suratmasuks');
+    }
+}
